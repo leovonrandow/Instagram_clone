89cca746da3d196a83e920c2f7c7f7a7d29efa32
Dirceu Belém <dirceu@fourtime.com>
2021-05-13 20:22:54 -0300

    Arredondando o Avatar

--- app/src/main/java/br/com/cotemig/socialcotemig/ui/adapters/FeedAdapter.kt
+++ app/src/main/java/br/com/cotemig/socialcotemig/ui/adapters/FeedAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.cotemig.socialcotemig.R
 import br.com.cotemig.socialcotemig.models.Post
 import coil.load
+import coil.transform.RoundedCornersTransformation
 
 class FeedAdapter(var context: Context, var list: List<Post>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
@@ -33,6 +34,20 @@ class FeedAdapter(var context: Context, var list: List<Post>) :
 
             var username = view.findViewById<TextView>(R.id.username)
             username.text = post.user
+
+            var locationName = view.findViewById<TextView>(R.id.locationName)
+            locationName.text = post.local
+
+            var date = view.findViewById<TextView>(R.id.date)
+            date.text = post.date
+
+            var description = view.findViewById<TextView>(R.id.description)
+            description.text = post.description
+
+            var avatar = view.findViewById<ImageView>(R.id.avatar)
+            avatar.load(post.avatar) {
+                transformations(RoundedCornersTransformation(50f))
+            }
 
             var postImage = view.findViewById<ImageView>(R.id.postImage)
             // esse método load só existe quando utilizamos o coil
